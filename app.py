78ab4ae416d3b09172f759afed0f49b478ad488a
Gaurav <88154142+ProgrammingWithGaurav@users.noreply.github.com>
2023-12-17 11:17:12 +0000

    Completed

---
+++ app.py
@@ -0,0 +1,17 @@
+from typing import Union
+from fastapi import FastAPI
+from test import PredictCustomerReview
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/predict")
+async def read_item(q: Union[str, None] = None):
+    pred = PredictCustomerReview(q)
+    print(pred)
+    return {"prediction": float(pred)}
---
+++ test.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import one_hot
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
+
+# load the model
+model = tf.keras.models.load_model('model.h5')
+
+vocab_size = 10000
+embeded_vector_size = 100
+max_length = 300
+
+def PredictCustomerReview(review):
+    data = one_hot(review, vocab_size)
+    sequences = [data]
+    padded_review = pad_sequences(sequences, maxlen=max_length, padding='post')
+    pred  = model.predict(padded_review)[0][0]
+    return pred # b/w 0-1
+
+if __name__ == '__main__':
+    # call the function
+    print(PredictCustomerReview("I love this product"))
+    
+    
+    
